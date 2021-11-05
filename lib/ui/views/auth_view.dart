7b42f1963501ec06b7f04f5747eddd95043a4199
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 19:11:02 +0530

    Added components to auth_view

--- lib/ui/views/auth_view.dart
+++ lib/ui/views/auth_view.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quasar_music/ui/shared/app_colors.dart';
+import 'package:quasar_music/ui/widgets/lazy_button.dart';
 import 'package:quasar_music/viewmodels/auth_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -12,9 +15,117 @@ class AuthView extends StatelessWidget {
     return ViewModelBuilder<AuthViewModel>.reactive(
       viewModelBuilder: () => AuthViewModel(),
       builder: (context, model, child) {
-        return const AnnotatedRegion<SystemUiOverlayStyle>(
+        return AnnotatedRegion<SystemUiOverlayStyle>(
           value: systemUiOverlayStyle,
-          child: Scaffold(),
+          child: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 16),
+              child: SizedBox.expand(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    Container(
+                      height: 160,
+                      width: 160,
+                      child: SvgPicture.asset(
+                        'lib/assets/images/quasar_logo.svg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Get your Money\nUnder Control',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Opacity(
+                      opacity: 0.5,
+                      child: Text(
+                        'Manage your expenses.\nSeamlessly.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    LazyButton(
+                      label: 'Sign Up with Email ID',
+                      onTap: () {},
+                      busy: model.busy,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 55,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              child: SvgPicture.asset(
+                                'lib/assets/images/g_logo.svg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Text(
+                              'Sign Up with Google',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Already have an account? ',
+                        children: [
+                          TextSpan(
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                            text: 'Sign In',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
