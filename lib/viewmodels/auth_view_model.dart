d6f1b319ba1f0614581ab0ad0d0094725ef24894
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 19:17:35 +0530

    Added nav functions to auth view model

--- lib/viewmodels/auth_view_model.dart
+++ lib/viewmodels/auth_view_model.dart
@@ -1,3 +1,16 @@
+import 'package:quasar_music/constants/route_names.dart';
+import 'package:quasar_music/locator.dart';
+import 'package:quasar_music/services/navigation_service.dart';
 import 'package:quasar_music/viewmodels/base_model.dart';
 
-class AuthViewModel extends BaseModel {}
+class AuthViewModel extends BaseModel {
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  signupUser() {
+    _navigationService.navigateTo(signupViewRoute);
+  }
+
+  signInUser() {
+    _navigationService.navigateTo(signInViewRoute);
+  }
+}
