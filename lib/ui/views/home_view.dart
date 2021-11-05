2a05f57b97531f9e34ec1612c92fe3433c019a59
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 18:47:44 +0530

    Created home view and viewmodel

---
+++ lib/ui/views/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quasar_music/ui/shared/app_colors.dart';
+import 'package:quasar_music/viewmodels/home_view_model.dart';
+import 'package:stacked/stacked.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      viewModelBuilder: () => HomeViewModel(),
+      builder: (context, model, child) {
+        return const AnnotatedRegion<SystemUiOverlayStyle>(
+          value: systemUiOverlayStyle,
+          child: Scaffold(),
+        );
+      },
+    );
+  }
+}
