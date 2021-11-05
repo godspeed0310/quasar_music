74f0b6caed5c2618077935a05c71cab193877495
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 18:45:26 +0530

    Created and added authentication_service to locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:quasar_music/services/authentication_service.dart';
 import 'package:quasar_music/services/navigation_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => AuthenticationService());
 }
