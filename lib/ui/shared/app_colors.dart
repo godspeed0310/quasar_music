cfea14d0b290d264522e4685098f6c6b9bd7033b
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 17:50:13 +0530

    Added SystemUIOverlayStyle to app color constants

--- lib/ui/shared/app_colors.dart
+++ lib/ui/shared/app_colors.dart
@@ -6,3 +6,9 @@ const Color primaryColor = Color(0xFF7B1FA2);
 const Color primaryColorDark = Color(0xFF4A0072);
 const Color scaffoldBackgroundColor = Color(0xFF121212);
 const Color lightgrey = Color(0xFF808080);
+const SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
+  statusBarColor: Colors.transparent,
+  statusBarIconBrightness: Brightness.light,
+  systemNavigationBarColor: scaffoldBackgroundColor,
+  systemNavigationBarIconBrightness: Brightness.light,
+);
