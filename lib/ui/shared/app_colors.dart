c96deac0d54c55ae5f825a8e96e221b6cfef67c3
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 17:18:13 +0530

    Added app color constants

---
+++ lib/ui/shared/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const Color primaryColorLight = Color(0xFFAE52D4);
+const Color primaryColor = Color(0xFF7B1FA2);
+const Color primaryColorDark = Color(0xFF4A0072);
+const Color scaffoldBackgroundColor = Color(0xFF121212);
+const Color lightgrey = Color(0xFF808080);
