13484712841bfafbcac820a6b49353a64ee5fb29
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-05 18:41:45 +0530

    Added applogo, name and circularProgressIndicator to startup view

--- lib/ui/views/startup_view.dart
+++ lib/ui/views/startup_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:quasar_music/ui/shared/app_colors.dart';
 import 'package:quasar_music/viewmodels/startup_view_model.dart';
 import 'package:stacked/stacked.dart';
@@ -12,9 +13,39 @@ class StartupView extends StatelessWidget {
     return ViewModelBuilder<StartupViewModel>.reactive(
       viewModelBuilder: () => StartupViewModel(),
       builder: (context, model, child) {
-        return const AnnotatedRegion<SystemUiOverlayStyle>(
+        return AnnotatedRegion<SystemUiOverlayStyle>(
           value: systemUiOverlayStyle,
-          child: Scaffold(),
+          child: Scaffold(
+            body: Center(
+              child: SizedBox.expand(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child:
+                          SvgPicture.asset('lib/assets/images/quasar_logo.svg'),
+                    ),
+                    const SizedBox(height: 40),
+                    const Text(
+                      'Quasar Music',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    const CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(primaryColor),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
