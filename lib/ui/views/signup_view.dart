3e507a1d7dc411bebce2779314de74f89ee242d4
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 20:54:48 +0530

    Initial UI Commit for signup_view

--- lib/ui/views/signup_view.dart
+++ lib/ui/views/signup_view.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quasar_music/ui/shared/app_colors.dart';
+import 'package:quasar_music/ui/widgets/input_field.dart';
+import 'package:quasar_music/ui/widgets/lazy_button.dart';
 import 'package:quasar_music/viewmodels/signup_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -9,12 +12,140 @@ class SignupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+
     return ViewModelBuilder<SignupViewModel>.reactive(
       viewModelBuilder: () => SignupViewModel(),
       builder: (context, model, child) {
-        return const AnnotatedRegion<SystemUiOverlayStyle>(
+        return AnnotatedRegion<SystemUiOverlayStyle>(
           value: systemUiOverlayStyle,
-          child: Scaffold(),
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              elevation: 0,
+              leading: GestureDetector(
+                onTap: () {},
+                child: const Icon(
+                  Icons.close,
+                ),
+              ),
+              actions: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: const Icon(Icons.help_outline),
+                  ),
+                )
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 16,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hey there!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 33,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 13,
+                  ),
+                  const Opacity(
+                    opacity: 0.5,
+                    child: Text(
+                      'Create a new account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  InputField(
+                    label: 'Email',
+                    spacing: 60,
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  InputField(
+                    label: 'Password',
+                    controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    isPassword: true,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  LazyButton(
+                    label: 'Continue',
+                    onTap: () {},
+                    busy: model.busy,
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'Don\'t have an account? ',
+                          children: [
+                            TextSpan(
+                              recognizer: TapGestureRecognizer()..onTap = () {},
+                              text: 'Sign In',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
