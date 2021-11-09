222b07f2d294c7d5fc182f04c13d9a28d26e96e0
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 21:24:05 +0530

    Implemented loginUser function to signinviewmodel

--- lib/viewmodels/signin_view_model.dart
+++ lib/viewmodels/signin_view_model.dart
@@ -1,10 +1,15 @@
 import 'package:quasar_music/constants/route_names.dart';
 import 'package:quasar_music/locator.dart';
+import 'package:quasar_music/services/authentication_service.dart';
+import 'package:quasar_music/services/dialog_service.dart';
 import 'package:quasar_music/services/navigation_service.dart';
 import 'package:quasar_music/viewmodels/base_model.dart';
 
 class SignInViewModel extends BaseModel {
   final NavigationService _navigationService = locator<NavigationService>();
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+  final DialogService _dialogService = locator<DialogService>();
 
   popPage() {
     _navigationService.pop();
@@ -12,5 +17,32 @@ class SignInViewModel extends BaseModel {
 
   navigateToSignup() {
     _navigationService.pushAndReplace(signupViewRoute);
+  }
+
+  Future loginUser({required String email, required String password}) async {
+    setBusy(true);
+    var result = await _authenticationService.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    setBusy(false);
+    if (result is bool) {
+      if (result) {
+        _navigationService.navigateAndReplaceTo(homeViewRoute);
+      } else {
+        await _dialogService.showDialog(
+          title: 'Couldn\'t Sign In',
+          description: 'General Signin error, please try again later.',
+        );
+        setBusy(false);
+      }
+    } else {
+      await _dialogService.showDialog(
+        title: 'Couldn\'t Sign In',
+        description: result,
+      );
+      setBusy(false);
+    }
   }
 }
