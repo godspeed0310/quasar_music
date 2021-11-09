679c992d6402bab0c0d52729a0aa5d74c3c737a0
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 20:55:59 +0530

    Added nav functions to signupViewModel

--- lib/viewmodels/signup_view_model.dart
+++ lib/viewmodels/signup_view_model.dart
@@ -1,3 +1,16 @@
+import 'package:quasar_music/constants/route_names.dart';
+import 'package:quasar_music/locator.dart';
+import 'package:quasar_music/services/navigation_service.dart';
 import 'package:quasar_music/viewmodels/base_model.dart';
 
-class SignupViewModel extends BaseModel {}
+class SignupViewModel extends BaseModel {
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  popPage() {
+    _navigationService.pop();
+  }
+
+  navigateToSignIn() {
+    _navigationService.pushAndReplace(signInViewRoute);
+  }
+}
