385a88a3b83ef9e884f8ca3c1abef89a72d5d879
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 22:42:12 +0530

    Initial home page view UI commit

--- lib/ui/views/Pageview Views/home_page_view.dart
+++ lib/ui/views/Pageview Views/home_page_view.dart
@@ -1,4 +1,6 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:quasar_music/ui/shared/app_colors.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({Key? key}) : super(key: key);
@@ -6,10 +8,84 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
-        child: Text(
-          'Home',
-          style: TextStyle(color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 100,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Hum\n your song!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    EvaIcons.optionsOutline,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(1000),
+              child: Ink(
+                height: 80,
+                width: 80,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: primaryGradient,
+                ),
+                child: const Center(
+                  child: Icon(
+                    EvaIcons.micOutline,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(12),
+              child: Ink(
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Reset',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
