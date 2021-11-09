719139b4679e5b26302f046e845e741c1c5b72c3
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 22:13:27 +0530

    Initial home_view UI Commit

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quasar_music/ui/shared/app_colors.dart';
@@ -9,12 +10,77 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PageController _pageController = PageController(initialPage: 0);
+
     return ViewModelBuilder<HomeViewModel>.reactive(
       viewModelBuilder: () => HomeViewModel(),
       builder: (context, model, child) {
-        return const AnnotatedRegion<SystemUiOverlayStyle>(
+        return AnnotatedRegion<SystemUiOverlayStyle>(
           value: systemUiOverlayStyle,
-          child: Scaffold(),
+          child: Scaffold(
+            body: PageView(
+                onPageChanged: (index) {
+                  model.updateIdx(index);
+                },
+                controller: _pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  Container(
+                    child: const Center(
+                      child: Text(
+                        'Home',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text(
+                        'My Playlist',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text(
+                        'Discover',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ]),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                _pageController.animateToPage(
+                  index,
+                  duration: const Duration(
+                    milliseconds: 200,
+                  ),
+                  curve: Curves.easeIn,
+                );
+              },
+              currentIndex: model.idx,
+              backgroundColor: scaffoldBackgroundColor,
+              unselectedItemColor: lightgrey,
+              selectedItemColor: primaryColorLight,
+              elevation: 10,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(EvaIcons.homeOutline),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(EvaIcons.musicOutline),
+                  label: 'My Playlist',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(EvaIcons.globe),
+                  label: 'Discover',
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
