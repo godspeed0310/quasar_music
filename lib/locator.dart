b4bc9ffdf5af293dd875d66db4319342ef4d336d
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 21:17:43 +0530

    Initialized dialog service in locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:quasar_music/services/authentication_service.dart';
+import 'package:quasar_music/services/dialog_service.dart';
 import 'package:quasar_music/services/navigation_service.dart';
 
 GetIt locator = GetIt.instance;
@@ -7,4 +8,5 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => DialogService());
 }
