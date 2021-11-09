40b7af9e0fc95e5b929692000fd9f8f880cf12e8
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-10 00:16:36 +0530

    Added Sound Graph Widget

---
+++ lib/ui/widgets/sound_graph.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SoundGraph extends StatelessWidget {
+  final List<double> soundData;
+  final Color unrecordedSoundColor;
+  final Color recordedSoundColor;
+  final double width;
+
+  const SoundGraph(
+      {Key? key,
+      required this.soundData,
+      required this.unrecordedSoundColor,
+      required this.recordedSoundColor,
+      this.width = 10})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      height: 100,
+      child: Align(
+        alignment: Alignment.center,
+        child: ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, index) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: width,
+                  height: soundData[index],
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: unrecordedSoundColor,
+                  ),
+                ),
+              ],
+            );
+          },
+          separatorBuilder: (BuildContext context, index) {
+            return index == soundData.length - 1
+                ? const SizedBox()
+                : SizedBox(
+                    width: width,
+                  );
+          },
+          itemCount: soundData.length,
+        ),
+      ),
+    );
+  }
+}
