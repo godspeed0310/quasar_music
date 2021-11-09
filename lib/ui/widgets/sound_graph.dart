96abd80b57d2a53af340cf504d33f0dbf1214595
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-10 01:40:54 +0530

    Replaced tweenAnimationBuilder with animationController in sound_graph

--- lib/ui/widgets/sound_graph.dart
+++ lib/ui/widgets/sound_graph.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SoundGraph extends StatelessWidget {
+class SoundGraph extends StatefulWidget {
   final List<double> soundData;
   final Color unrecordedSoundColor;
   final Color recordedSoundColor;
@@ -15,21 +15,51 @@ class SoundGraph extends StatelessWidget {
       : super(key: key);
 
   @override
+  _SoundGraphState createState() => _SoundGraphState();
+}
+
+class _SoundGraphState extends State<SoundGraph>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late final Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+    _animation = IntTween(begin: 0, end: 1).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      height: 100,
-      child: Align(
-        alignment: Alignment.center,
-        child: TweenAnimationBuilder(
-          tween: Tween<double>(begin: 0, end: 1),
-          duration: const Duration(seconds: 10),
-          builder: (context, double value, child) {
-            return ShaderMask(
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Container(
+          width: double.maxFinite,
+          height: 100,
+          child: Align(
+            alignment: Alignment.center,
+            child: ShaderMask(
               shaderCallback: (rect) {
-                return LinearGradient(
-                    colors: [recordedSoundColor, unrecordedSoundColor],
-                    stops: [value, value]).createShader(rect);
+                return LinearGradient(colors: [
+                  widget.recordedSoundColor,
+                  widget.unrecordedSoundColor
+                ], stops: [
+                  _controller.value,
+                  _controller.value
+                ]).createShader(rect);
               },
               child: ListView.separated(
                 physics: const NeverScrollableScrollPhysics(),
@@ -41,29 +71,29 @@ class SoundGraph extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Container(
-                        width: width,
-                        height: soundData[index],
+                        width: widget.width,
+                        height: widget.soundData[index],
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
-                          color: unrecordedSoundColor,
+                          color: widget.unrecordedSoundColor,
                         ),
                       ),
                     ],
                   );
                 },
                 separatorBuilder: (BuildContext context, index) {
-                  return index == soundData.length - 1
+                  return index == widget.soundData.length - 1
                       ? const SizedBox()
                       : SizedBox(
-                          width: width,
+                          width: widget.width,
                         );
                 },
-                itemCount: soundData.length,
+                itemCount: widget.soundData.length,
               ),
-            );
-          },
-        ),
-      ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
