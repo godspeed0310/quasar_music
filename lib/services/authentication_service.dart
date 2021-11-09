fa8cd8619b6f893129c966c7c3d1598db125b07c
Dharam Soni <20190802088@dypiu.ac.in>
2021-11-09 21:22:26 +0530

    Added signup and signin functions to authservice

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -3,6 +3,36 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  Future signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var authResult = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return authResult.user != null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var authResult = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return authResult.user != null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
   Future<bool> isUserLoggedIn() async {
     var user = await _firebaseAuth.currentUser;
     return user != null;
