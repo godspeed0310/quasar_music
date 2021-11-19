37b0f199b8f184978bbcff519eae6080f1ca531b
godspeed0310 <20190802088@dypiu.ac.in>
2021-11-19 22:02:39 +0530

    Created song datamodel

---
+++ lib/models/song_model.dart
@@ -0,0 +1,11 @@
+class SongModel {
+  final String title;
+  final List<String> artists;
+  final String imgUri;
+
+  SongModel({
+    required this.title,
+    required this.artists,
+    required this.imgUri,
+  });
+}
