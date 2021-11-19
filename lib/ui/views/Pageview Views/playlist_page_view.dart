349863b6053883f890bd592036576b14c45c7f14
godspeed0310 <20190802088@dypiu.ac.in>
2021-11-19 18:47:50 +0530

    Removed spotify API dependency and created initialized playlists page

--- lib/ui/views/Pageview Views/playlist_page_view.dart
+++ lib/ui/views/Pageview Views/playlist_page_view.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PlaylistPageView extends StatelessWidget {
   const PlaylistPageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text(
-          'My Playlist',
-          style: TextStyle(color: Colors.white),
-        ),
+    return SizedBox.expand(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 250,
+            width: 250,
+            child: SvgPicture.asset(
+              'lib/assets/images/no_music_illustration.svg',
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'No playlists found on your account.\nCreate a new playlist to see it here.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          )
+        ],
       ),
     );
   }
