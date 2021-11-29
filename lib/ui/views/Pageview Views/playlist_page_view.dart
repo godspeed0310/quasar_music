938f79f96df112bc24f7c42ccca4d91864436c5d
godspeed0310 <20190802088@dypiu.ac.in>
2021-11-29 20:38:50 +0530

    Fixed bug with no songs in playlist page view

--- lib/ui/views/Pageview Views/playlist_page_view.dart
+++ lib/ui/views/Pageview Views/playlist_page_view.dart
@@ -54,146 +54,152 @@ class _PlaylistPageViewState extends State<PlaylistPageView> {
           var songDoc = snapshot.data!.data();
           var songs = (songDoc as Map)['favourites'];
 
-          return SafeArea(
-            child: Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Your favourites',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 14,
-                    child: ListView.builder(
-                      itemCount: songs != null ? songs.length : 0,
-                      itemBuilder: (_, int index) {
-                        return ListTile(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => SongView(
-                                  song: SongModel.fromMap(
-                                    songs[index],
-                                  ),
+          return songs.length > 0
+              ? SafeArea(
+                  child: Scaffold(
+                    body: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: const Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Your favourites',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
                                 ),
                               ),
-                            );
-                          },
-                          leading: Container(
-                            height: 50,
-                            width: 50,
-                            child: CachedNetworkImage(
-                              imageUrl: songs[index]['artworkRawUrl'],
                             ),
-                          ),
-                          title: Text(
-                            songs[index]['title'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Text(
-                            songs[index]['artistName'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              floatingActionButton: FloatingActionButton(
-                backgroundColor: primaryColor,
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('Create a new playlist'),
-                      content: TextField(
-                        controller: inputController,
-                        decoration: const InputDecoration(
-                          hintText: 'Name your playlist',
-                        ),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text(
-                            'Cancel',
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            _firestoreService.createPlaylist(
-                              inputController.text,
-                              _authenticationService.currentUser.uid,
-                            );
-                            Navigator.pop(context);
-                          },
-                          child: const Text(
-                            'OK',
+                        Expanded(
+                          flex: 14,
+                          child: ListView.builder(
+                            itemCount: songs != null ? songs.length : 0,
+                            itemBuilder: (_, int index) {
+                              return ListTile(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => SongView(
+                                        song: SongModel.fromMap(
+                                          songs[index],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                leading: Container(
+                                  height: 50,
+                                  width: 50,
+                                  child: CachedNetworkImage(
+                                    imageUrl: songs[index]['artworkRawUrl'],
+                                  ),
+                                ),
+                                title: Text(
+                                  songs[index]['title'],
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  songs[index]['artistName'],
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
                       ],
                     ),
-                  );
-                },
-                child: const Icon(
-                  EvaIcons.plus,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          );
-        } else {
-          return SizedBox.expand(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 250,
-                  width: 250,
-                  child: SvgPicture.asset(
-                    'lib/assets/images/no_music_illustration.svg',
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  'No playlists found on your account.\nCreate a new playlist to see it here.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
+                    floatingActionButton: FloatingActionButton(
+                      backgroundColor: primaryColor,
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Create a new playlist'),
+                            content: TextField(
+                              controller: inputController,
+                              decoration: const InputDecoration(
+                                hintText: 'Name your playlist',
+                              ),
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  'Cancel',
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  _firestoreService.createPlaylist(
+                                    inputController.text,
+                                    _authenticationService.currentUser.uid,
+                                  );
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  'OK',
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: const Icon(
+                        EvaIcons.plus,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 )
-              ],
+              : SizedBox.expand(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 250,
+                        width: 250,
+                        child: SvgPicture.asset(
+                          'lib/assets/images/no_music_illustration.svg',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Text(
+                        'No playlists found on your account.\nCreate a new playlist to see it here.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: Text('No data found'),
             ),
           );
         }
